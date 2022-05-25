f5447804128bc5329b4e8f2958fbada5653589c8
Migisha098 <100316803+Migisha098@users.noreply.github.com>
2022-05-25 22:18:20 +0200

    Initial commit

---
+++ src/RectangleArea.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RectangleArea {
+   double a,b,area ;
+
+
+   void getData()throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       System.out.println("The program will calculate the area of rectangle ");
+       System.out.println("Enter the length of side a :");
+       a = Double.parseDouble(br.readLine());
+       System.out.println("Enter the length of the side b: ");
+       b = Double.parseDouble(br.readLine());
+   }
+   void computeField() { area = a*b;}
+
+    void fieldDisplay (){
+        System.out.print("The area of the rectangle with the side a = ");
+        System.out.printf("%2.2f",a);
+        System.out.print(" and side b = ");
+        System.out.printf("%2.2f ",b);
+        System.out.print(" is = ");
+        System.out.printf("%2.2f.\n", area);
+    }
+}
---
+++ src/RectangleDemo.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class RectangleDemo {
+    public static void main(String[] args)throws IOException {
+        RectangleArea obj = new RectangleArea();
+                obj.getData();
+               obj.computeField();
+               obj.fieldDisplay();
+    }
+}
